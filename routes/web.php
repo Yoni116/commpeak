2171dbbf60198b99e5c2732ed05328c9128cd641
yoni116@gmail.com <Maymon300>
2016-12-08 17:03:31 +0200

    add general route to contact form

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/',
+    ['as' => 'contact_create', 'uses' => 'ContactController@create']);
 
 Route::get('contact',
     ['as' => 'contact_create', 'uses' => 'ContactController@create']);
